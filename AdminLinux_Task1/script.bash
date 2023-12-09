f2698052e3e1c55db9e534944636b852d87544dd
FatmaEzzat2 <Fatmaex15@gmail.com>
2023-12-10 00:15:56 +0200

    Add AdminLinux_Task1 directory with script for Task 1

---
+++ AdminLinux_Task1/script.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd 
+mkdir myDirectory
+cd myDirectory
+mkdir secondDirectoy
+cd secondDirectoy
+touch myNotePaper
+cp myNotePaper ../../myDirectory/
+cd ..
+mv myNotePaper myOldNotePaper
+
+
+
+ 
+
